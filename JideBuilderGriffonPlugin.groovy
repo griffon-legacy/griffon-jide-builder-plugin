c4eb643d871124f3324eaa0a2f6d46e945970854
aalmiray <aalmiray@gmail.com>
2012-02-13 18:51:07 +0100

    Upgrade to Griffon 0.9.5

--- JideBuilderGriffonPlugin.groovy
+++ JideBuilderGriffonPlugin.groovy
@@ -18,7 +18,7 @@
  * @author Andres Almiray
  */
 class JideBuilderGriffonPlugin {
-    String version = '0.5'
+    String version = '0.7'
     // the version or versions of Griffon the plugin is designed for
     String griffonVersion = '0.9.5 > *'
     // the other plugins this plugin depends on
